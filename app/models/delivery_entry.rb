f93e04a4cf5c57e4108a78a08f75a334066a27f6
DominikDranz <dominik.dranz@EMAIL.com>
2018-05-03 11:00:17 -0400

    set the root to index page, add validation for attributes

--- app/models/delivery_entry.rb
+++ app/models/delivery_entry.rb
@@ -1,2 +1,10 @@
 class DeliveryEntry < ApplicationRecord
+	validates :date, presence: true
+	validates :in, presence: true
+	validates :company, presence: true
+	validates :plate_vehicle, presence: true
+	validates :plate_trailer, presence: true
+	validates :plate_state, presence: true
+	validates :initials, presence: true
+	validates :pass_num, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :delivery_entries
+  root 'delivery_entries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
